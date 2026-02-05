01d1433b7e331147b06a1a25facdf189b5632bf0
WZYivan <2270060975@qq.com>
2026-02-05 09:40:43 +0800

    feat: add global enum class represeting status

---
+++ src/lga/impl/Constans.hpp
@@ -0,0 +1,16 @@
+#ifndef M_libga_impl_constants
+#define M_libga_impl_constants
+
+#include <lga/Base>
+
+M_libga_begin
+
+    enum class Iterative_Algo_Info {
+        Success,
+        Not_Converged,
+        Pre_Unsatisfied
+    };
+
+M_libga_end
+
+#endif
