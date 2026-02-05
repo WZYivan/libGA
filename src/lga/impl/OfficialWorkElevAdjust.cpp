65ef929061c8e5fb6a7ef41552a4dd46cf2233c1
WZYivan <2270060975@qq.com>
2026-02-05 17:48:54 +0800

    feat: :sparkles: add frame and algorithms for offical work

---
+++ src/lga/impl/OfficialWorkElevAdjust.cpp
@@ -0,0 +1,212 @@
+#include <numeric>
+#include <algorithm>
+
+#include <DataFrame/DataFrameFinancialVisitors.h>
+
+#include <lga/OfficialWork>
+
+M_libga_begin
+
+    DataFrame
+    closedElevAdjust(
+        std::vector<double> &p_distances,
+        std::vector<double> &p_diff,
+        double p_beg,
+        int p_correction_precision,
+        std::function<double(double)> p_tolerence_fn)
+{
+    size_t size = p_distances.size();
+
+    if (size != p_diff.size())
+    {
+        throw std::invalid_argument(
+            std::format(
+                "size {} != {}",
+                size,
+                p_diff.size()));
+    }
+
+    double closure = std::accumulate(p_diff.begin(), p_diff.end(), 0.0) * 1000;
+    double tolerence = internal::calcTolerence(p_distances, p_tolerence_fn);
+
+    DataFrameColumnIndices indices(size + 1);
+    std::iota(indices.begin(), indices.end(), 0);
+
+    DataFrame df;
+    df.load_index(std::move(indices));
+    df.load_column("distance", std::move(p_distances));
+    df.load_column("diff", std::move(p_diff));
+
+    hmdf::SumVisitor<double> sum_v(true);
+    df.visit<double>("distance", sum_v);
+    double distance_sum = sum_v.get_result();
+    df.visit<double>("diff", sum_v);
+    double diff_sum = sum_v.get_result();
+
+    double correction_sum = cut(
+        -1000 * diff_sum,
+        p_correction_precision);
+    double mean_correction = cut(
+        correction_sum / (distance_sum),
+        p_correction_precision);
+
+    std::vector<double> corrections(size);
+    const auto &distances =
+        df.get_column<double>("distance");
+    for (auto idx = 0uz; idx != size; ++idx)
+    {
+        corrections.at(idx) =
+            cut(
+                distances.at(idx) * mean_correction,
+                p_correction_precision);
+    }
+    internal::adjustSumToTarget(
+        corrections,
+        std::accumulate(
+            corrections.begin(),
+            corrections.end(),
+            0.0),
+        correction_sum,
+        p_correction_precision);
+
+    std::vector<double> corrected_diff(size);
+    const auto &diff = df.get_column<double>("diff");
+    for (auto idx = 0uz; idx != size; ++idx)
+    {
+        corrected_diff.at(idx) =
+            diff.at(idx) + corrections.at(idx) / 1000;
+    }
+
+    std::vector<double> elev(size + 1);
+    elev.at(0) = p_beg;
+    for (auto idx = 0uz; idx != size; ++idx)
+    {
+        elev.at(idx + 1) =
+            elev.at(idx) + corrected_diff.at(idx);
+    }
+
+    df.load_column("correction", std::move(corrections));
+    df.load_column("corrected diff", std::move(corrected_diff));
+    df.visit<double>("corrected diff", sum_v);
+    double corrected_dif_sum = zeroOr(sum_v.get_result());
+    df.load_column("elev", std::move(elev));
+
+    std::vector<std::string> sum_idx_col{
+        "distances", "diff", "correction", "corrected diff"};
+    std::vector<double> sum_col{
+        distance_sum, diff_sum, correction_sum, corrected_dif_sum};
+    df.load_column("sum of(name)", std::move(sum_idx_col));
+    df.load_column("sum of(val)", std::move(sum_col));
+
+    std::vector<std::string> info_idx_col{
+        "closure", "tolerence", "mean correction"};
+    std::vector<double> info_col{
+        closure, tolerence, mean_correction};
+    df.load_column("info(name)", std::move(info_idx_col));
+    df.load_column("info(val)", std::move(info_col));
+
+    return df;
+}
+
+DataFrame
+attachedElevAdjust(
+    std::vector<double> &p_distances,
+    std::vector<double> &p_diff,
+    double p_beg,
+    double p_end,
+    int p_correction_precision,
+    std::function<double(double)> p_tolerence_fn)
+{
+    size_t size = p_distances.size();
+
+    if (size != p_diff.size())
+    {
+        throw std::invalid_argument(
+            std::format(
+                "size {} != {}",
+                size,
+                p_diff.size()));
+    }
+
+    double closure = std::accumulate(p_diff.begin(), p_diff.end(), 0.0) - (p_end - p_beg);
+    double tolerence = internal::calcTolerence(p_distances, p_tolerence_fn);
+
+    DataFrameColumnIndices indices(size + 1);
+    std::iota(indices.begin(), indices.end(), 0);
+
+    DataFrame df;
+    df.load_index(std::move(indices));
+    df.load_column("distance", std::move(p_distances));
+    df.load_column("diff", std::move(p_diff));
+
+    hmdf::SumVisitor<double> sum_v(true);
+    df.visit<double>("distance", sum_v);
+    double distance_sum = sum_v.get_result();
+    df.visit<double>("diff", sum_v);
+    double diff_sum = sum_v.get_result();
+
+    double correction_sum = cut(
+        -1000 * closure,
+        p_correction_precision);
+    double mean_correction = cut(
+        correction_sum / (distance_sum),
+        p_correction_precision);
+
+    std::vector<double> corrections(size);
+    const auto &distances =
+        df.get_column<double>("distance");
+    for (auto idx = 0uz; idx != size; ++idx)
+    {
+        corrections.at(idx) =
+            cut(
+                distances.at(idx) * mean_correction,
+                p_correction_precision);
+    }
+    internal::adjustSumToTarget(
+        corrections,
+        std::accumulate(
+            corrections.begin(),
+            corrections.end(),
+            0.0),
+        correction_sum,
+        p_correction_precision);
+
+    std::vector<double> corrected_diff(size);
+    const auto &diff = df.get_column<double>("diff");
+    for (auto idx = 0uz; idx != size; ++idx)
+    {
+        corrected_diff.at(idx) =
+            diff.at(idx) + corrections.at(idx) / 1000;
+    }
+
+    std::vector<double> elev(size + 1);
+    elev.at(0) = p_beg;
+    for (auto idx = 0uz; idx != size; ++idx)
+    {
+        elev.at(idx + 1) =
+            elev.at(idx) + corrected_diff.at(idx);
+    }
+
+    df.load_column("correction", std::move(corrections));
+    df.load_column("corrected diff", std::move(corrected_diff));
+    df.visit<double>("corrected diff", sum_v);
+    double corrected_dif_sum = zeroOr(sum_v.get_result());
+    df.load_column("elev", std::move(elev));
+
+    std::vector<std::string> sum_idx_col{
+        "distances", "diff", "correction", "corrected diff"};
+    std::vector<double> sum_col{
+        distance_sum, diff_sum, correction_sum, corrected_dif_sum};
+    df.load_column("sum of(name)", std::move(sum_idx_col));
+    df.load_column("sum of(val)", std::move(sum_col));
+
+    std::vector<std::string> info_idx_col{
+        "closure", "tolerence", "mean correction", "expect target elev"};
+    std::vector<double> info_col{
+        closure, tolerence, mean_correction, p_end};
+    df.load_column("info(name)", std::move(info_idx_col));
+    df.load_column("info(val)", std::move(info_col));
+
+    return df;
+}
+M_libga_end
---
+++ src/lga/impl/OfficialWorkUtils.cpp
@@ -0,0 +1,54 @@
+#include <lga/OfficialWork>
+
+M_libga_begin
+
+    namespace param
+{
+    const Official_Work official_work = {};
+}
+
+namespace internal
+{
+    void adjustSumToTarget(
+        std::vector<double> &p_vec,
+        double p_sum,
+        double p_target,
+        int p_precesion)
+    {
+        double unit =
+            p_precesion > 0 ? std::pow(0.1, p_precesion) : 1.0;
+        double dif = cut(p_sum - p_target, p_precesion);
+        unit = cut(unit, p_precesion);
+        bool dif_sign = std::signbit(dif);
+        unit = dif_sign ? -unit : unit;
+        int count = dif / unit;
+
+        while (count-- > 0)
+        {
+            size_t idx = 0;
+
+            if (dif_sign)
+            {
+                auto min = std::min_element(p_vec.begin(), p_vec.end());
+                idx = std::distance(p_vec.begin(), min);
+            }
+            else
+            {
+                auto max = std::max_element(p_vec.begin(), p_vec.end());
+                idx = std::distance(p_vec.begin(), max);
+            }
+
+            p_vec.at(idx) = cut(p_vec.at(idx) - unit, p_precesion);
+        }
+    }
+
+    double calcTolerence(
+        const std::vector<double> &p_vec,
+        std::function<double(double)> p_fn)
+    {
+        return p_fn(std::accumulate(p_vec.begin(), p_vec.end(), 0.0));
+    }
+
+}
+
+M_libga_end
