27befd3eee603d8ccf2e3945761cf3a2b500aa2f
WZYivan <2270060975@qq.com>
2026-02-06 17:47:09 +0800

    perf: remove unnecessary loop

--- src/lga/impl/OfficialWorkUtils.cpp
+++ src/lga/impl/OfficialWorkUtils.cpp
@@ -23,23 +23,35 @@ namespace internal
         unit = dif_sign ? -unit : unit;
         int count = dif / unit;
 
-        while (count-- > 0)
-        {
-            size_t idx = 0;
+        size_t idx = 0;
 
-            if (dif_sign)
+        if (eqApprox(dif, 0.0))
+        {
+            return;
+        }
+
+        if (dif_sign)
+        {
+            while (count-- > 0)
             {
                 auto min = std::min_element(p_vec.begin(), p_vec.end());
                 idx = std::distance(p_vec.begin(), min);
+                double val = cut(p_vec.at(idx) - unit, p_precesion);
+                p_vec.at(idx) = val;
             }
-            else
+        }
+        else
+        {
+            while (count-- > 0)
             {
                 auto max = std::max_element(p_vec.begin(), p_vec.end());
                 idx = std::distance(p_vec.begin(), max);
+                double val = cut(p_vec.at(idx) - unit, p_precesion);
+                p_vec.at(idx) = val;
             }
-
-            p_vec.at(idx) = cut(p_vec.at(idx) - unit, p_precesion);
         }
+
+        return;
     }
 
     double calcTolerence(
